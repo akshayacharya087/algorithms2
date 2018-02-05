c8008c04e880f3a6d90c7d8bacfd538fc836d57c
Yeremy Turcios <yeremyturcios@gmail.com>
2018-02-05 06:59:31 -0700

    Fixing bugs.

--- src/SAP.java
+++ src/SAP.java
@@ -1,107 +1,126 @@
+import edu.princeton.cs.algs4.DepthFirstOrder;
 import edu.princeton.cs.algs4.In;
 
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
+
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class SAP {
+    private final Digraph G;
+    private final Iterable<Integer> topoSort;
 
-	private final Digraph digraph;
-	private final BreathFirstPath breathFirstPath;
+    private class Ancestor {
+        int distance;
+        int ancestor;
 
-	/**
-	 * Constructor takes a digraph (not necessarily a DAG)
-	 * @param g
-	 */
-	public SAP(Digraph g) {
-		digraph = new Digraph(g);
-		breathFirstPath = new BreathFirstPath(g);
-	}
-	
-	/**
-	 *  Length of shortest ancestral path between v and w; -1 if no such path
-	 * @param v
-	 * @param w
-	 * @return
-	 */
-	public int length(int v, int w) {
-		checkInput(v);
-		checkInput(w);
-		breathFirstPath.bfs(v, w);
-		return breathFirstPath.getLength();
-	}
-	
-	 /**
-	  * Checks that the input is between 0 and digraph.V()-1
-	  */
-	private void checkInput(int vertex) {
-		if (!(vertex >= 0 && vertex <= digraph.V() -1)) throw new IndexOutOfBoundsException(); 
-		
-	}
+        Ancestor(int ancestor, int distance) {
+            this.ancestor = ancestor;
+            this.distance = distance;
+        }
+    }
 
-	/**
-	 * A common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-	 * @param v
-	 * @param w
-	 * @return
-	 */
-	public int ancestor(int v, int w) {
-		checkInput(v);
-		checkInput(w);
-		breathFirstPath.bfs(v, w);
-		return breathFirstPath.getAncestor();
-	}
-	
-	/**
-	 * Length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
-	 * @param v
-	 * @param w
-	 * @return
-	 */
-	public int length(Iterable<Integer> v, Iterable<Integer> w) {	
-		checkInputArray(v);
-		checkInputArray(w);
-		
-		breathFirstPath.bfs(v, w);
-		return breathFirstPath.getLength();
-	}
-	
-	/**
-	 * Checks that all integers in the array are between 0 and digraph.V() - 1
-	 */
-	private void checkInputArray(Iterable<Integer> vertexList) {
-		for (int vertex : vertexList) {
-			checkInput(vertex);
-		}
-		
-	}
+    // constructor takes a digraph (not necessarily a DAG)
+    // complexity due to DFS traversal O(V + E)
+    public SAP(Digraph G) {
+        if (G == null) {
+            throw new IllegalArgumentException();
+        }
+        this.G = new Digraph(G);
+        DepthFirstOrder traversal = new DepthFirstOrder(G);
+        topoSort = traversal.reversePost();
+    }
 
-	/**
-	 * A common ancestor that participates in shortest ancestral path; -1 if no such path
-	 * @param v
-	 * @param w
-	 * @return
-	 */
-	public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-		breathFirstPath.bfs(v, w);
-		return breathFirstPath.getAncestor();
-	}
-		
-	
-	/**
-	 * Do unit testing of this class
-	 * @param args
-	 */
-	public static void main(String args[]) {
-		In in = new In("/resources/digraph5.txt");
-	    Digraph G = new Digraph(in);
-	    SAP sap = new SAP(G);
-        int v = 21;
-        int w = 15;
-        int length   = sap.length(v, w);
-        int ancestor = sap.ancestor(v, w);
-        StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
-	    
-	}
-	
+    private boolean isValidVertex(int v) {
+        return (v >= 0) && (v <= G.V() - 1);
+    }
+
+    private void validateArgs(Iterable<Integer> v, Iterable<Integer> w) {
+        if ((v == null) || (w == null)) {
+            throw new IllegalArgumentException("Null argument");
+        }
+        for (int orig : v) {
+            if (!isValidVertex(orig)) {
+                throw new IllegalArgumentException("Invalid origin vertex " + orig);
+            }
+        }
+        for (int dest : w) {
+            if (!isValidVertex(dest)) {
+                throw new IllegalArgumentException("Invalid destination vertex " + dest);
+            }
+        }
+    }
+
+    // Complexity: 2 * O(V + E) + O(V) due to 2 BFS traversals + visiting V roots (worst case)
+    private Ancestor findCommonAncestor(Iterable<Integer> v, Iterable<Integer> w) {
+        validateArgs(v, w);
+
+        BreadthFirstDirectedPaths origPaths = new BreadthFirstDirectedPaths(G, v);
+        BreadthFirstDirectedPaths destPaths = new BreadthFirstDirectedPaths(G, w);
+
+        int minDistance = Integer.MAX_VALUE;
+        Ancestor ancestor = null;
+        for (int root : topoSort) {
+            if (origPaths.hasPathTo(root) && destPaths.hasPathTo(root)) {
+                int distance = origPaths.distTo(root) + destPaths.distTo(root);
+                if (distance >= 0 && distance < minDistance) {
+                    minDistance = distance;
+                    ancestor = new Ancestor(root, distance);
+                }
+            }
+        }
+        return ancestor;
+    }
+
+    private static List<Integer> listWith(int value) {
+        List<Integer> list = new LinkedList<>();
+        list.add(value);
+        return list;
+    }
+
+    // length of shortest ancestral path between v and w; -1 if no such path
+    public int length(int v, int w) {
+        return length(listWith(v), listWith(w));
+    }
+
+    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+    public int ancestor(int v, int w) {
+        return ancestor(listWith(v), listWith(w));
+    }
+
+    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+    public int length(Iterable<Integer> v, Iterable<Integer> w) {
+        Ancestor common = findCommonAncestor(v, w);
+        if (common != null) {
+            return common.distance;
+        }
+        return -1;
+    }
+
+    // a common ancestor that participates in shortest ancestral path; -1 if no such path
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        Ancestor common = findCommonAncestor(v, w);
+        if (common != null) {
+            return common.ancestor;
+        }
+        return -1;
+    }
+
+    // do unit testing of this class
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+        SAP sap = new SAP(G);
+        while (!StdIn.isEmpty()) {
+            int v = StdIn.readInt();
+            int w = StdIn.readInt();
+            int length = sap.length(v, w);
+            int ancestor = sap.ancestor(v, w);
+            StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+        }
+    }
 }
--- src/WordNet.java
+++ src/WordNet.java
@@ -1,152 +1,180 @@
-import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.RedBlackBST;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
+import edu.princeton.cs.algs4.Topological;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class WordNet {
 
-	private final HashMap<String, ArrayList<Integer>> nounsMap;	
-	private final HashMap<Integer, String> synsetsMap;
-
-	private final Digraph wordNet;	
+	private final RedBlackBST<String, List<Synonym>> symbolTable = new RedBlackBST<>();
 	private final SAP sap;
-	/**
-	 * Constructor takes the name of the two input files
-	 * 
-	 * @param synsets
-	 * @param hypernyms
-	 */
+	private final Synonym[] synonyms;
+
+	private class Synonym {
+		int id;
+		String[] synset;
+
+		Synonym(int id, String[] synset) {
+			this.id = id;
+			this.synset = synset;
+		}
+	}
+
+	// constructor takes the name of the two input files
 	public WordNet(String synsets, String hypernyms) {
-		if (synsets == null || hypernyms == null) throw new NullPointerException();
-		
-		In in = new In(synsets);
-			
-		nounsMap = new HashMap<>();
-		synsetsMap = new HashMap<>();
-		int synsetCounter = 0;
-		
-		// Filling the synsets, and nouns
-		while (in.hasNextLine()) {
-			String[] elements = in.readLine().split(",");
-			int synsetId = Integer.parseInt(elements[0]);
-			String[] nouns = elements[1].split(" ");
-			
-			// Filling nounsMap
-			for (String noun : nouns) {
-				if (nounsMap.containsKey(noun)) {
-					nounsMap.get(noun).add(synsetId);
-				}
-				else {
-					ArrayList<Integer> synsetsIds = new ArrayList<>();
-					synsetsIds.add(synsetId);
-					nounsMap.put(noun, synsetsIds);
-				}
-			}
-			
-			synsetsMap.put(new Integer(synsetId), elements[1]);
-			
-			
-			synsetCounter++;
+		if ((synsets == null) || (hypernyms == null)) {
+			throw new IllegalArgumentException();
 		}
-		
-		// Creating the digraph
-		wordNet = new Digraph(synsetCounter);
-		
-		in = new In(hypernyms);
-		
-		while(in.hasNextLine()) {
-			String[] elements = in.readLine().split(",");
-			for (int i = 1; i < elements.length; i++) {
-				wordNet.addEdge(Integer.parseInt(elements[0]), Integer.parseInt(elements[i]));
+
+		In synsetsIn = new In(synsets);
+		int maxId = -1;
+		while (!synsetsIn.isEmpty()) {
+			String line = synsetsIn.readLine();
+			String[] values = line.split(",");
+			String[] words = values[1].split(" ");
+			maxId = Integer.parseInt(values[0]);
+			Synonym syn = new Synonym(maxId, words);
+			for (String word : words) {
+				List<Synonym> lookup = symbolTable.get(word);
+				if (lookup == null) {
+					lookup = new LinkedList<>();
+				}
+				lookup.add(syn);
+				symbolTable.put(word, lookup);
 			}
-			
 		}
-		
-		// Creating the SAP object
-		sap = new SAP(wordNet);		
+
+		synonyms = new Synonym[maxId + 1];
+		for (String key : symbolTable.keys()) {
+			List<Synonym> symbols = symbolTable.get(key);
+			for (Synonym s : symbols) {
+				synonyms[s.id] = s;
+			}
+		}
+
+		Digraph graph = new Digraph(maxId + 1);
+		In hypernymsIn = new In(hypernyms);
+		while (!hypernymsIn.isEmpty()) {
+			String line = hypernymsIn.readLine();
+			String[] values = line.split(",");
+			int n = Integer.parseInt(values[0]);
+			for (int i = 1; i < values.length; i++) {
+				int v = Integer.parseInt(values[i]);
+				graph.addEdge(n, v);
+			}
+		}
+		Topological traversal = new Topological(graph);
+		if (!traversal.hasOrder()) {
+			throw new IllegalArgumentException("Graph has a cycle");
+		}
+		int root = -1;
+		for (int v = 0; v < graph.V(); v++) {
+			if (graph.outdegree(v) == 0) {
+				if (root >= 0) {
+					throw new IllegalArgumentException("Graph has multiple roots");
+				}
+				root = v;
+			}
+		}
+		sap = new SAP(graph);
 	}
 
-	/**
-	 * Returns all WordNet nouns
-	 * 
-	 * @return
-	 */
+	// returns all WordNet nouns
 	public Iterable<String> nouns() {
-		return nounsMap.keySet();
+		return symbolTable.keys();
 	}
 
-	/**
-	 * Is the word a WordNet noun?
-	 * 
-	 * @param word
-	 * @return
-	 */
-	public boolean isNoun(String word) {		
-		checkInput(word);
-		return (nounsMap.containsKey(word));
+	private int count() {
+		return symbolTable.size();
 	}
 
-	/**
-	 *  Distance between nounA and nounB (defined below)
-	 * @param nounA
-	 * @param nounB
-	 * @return
-	 */
+	// is the word a WordNet noun?
+	public boolean isNoun(String word) {
+		if (word == null) {
+			throw new IllegalArgumentException();
+		}
+		return symbolTable.contains(word);
+	}
+
+	private List<Integer> matchingKeys(String word) {
+		List<Integer> keys = new LinkedList<>();
+		for (Synonym s : symbolTable.get(word)) {
+			keys.add(s.id);
+		}
+		return keys;
+	}
+
+	// distance between nounA and nounB (defined below)
 	public int distance(String nounA, String nounB) {
-		checkInputs(nounA, nounB);
-		return sap.length(nounsMap.get(nounA), nounsMap.get(nounB));
+		if ((nounA == null) || (nounB == null)) {
+			throw new IllegalArgumentException();
+		}
+		if (!isNoun(nounA) || !(isNoun(nounB))) {
+			throw new IllegalArgumentException("Arguments must be nouns");
+		}
+
+		return sap.length(matchingKeys(nounA), matchingKeys(nounB));
 	}
-	
-	
-	/**
-	 * A synset (second field of synsets.txt) that is the common ancestor of
-	 * nounA and nounB in a shortest ancestral path (defined below)
-	 * 
-	 * @param nounA
-	 * @param nounB
-	 * @return
-	 */
+
+	// a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+	// in a shortest ancestral path (defined below)
 	public String sap(String nounA, String nounB) {
-		checkInputs(nounA, nounB);
-		int ancestor = sap.ancestor(nounsMap.get(nounA), nounsMap.get(nounB));
-		return synsetsMap.get(ancestor);
+		if ((nounA == null) || (nounB == null)) {
+			throw new IllegalArgumentException();
+		}
+		if (!isNoun(nounA) || !(isNoun(nounB))) {
+			throw new IllegalArgumentException("Arguments must be nouns");
+		}
+
+		int ancestor = sap.ancestor(matchingKeys(nounA), matchingKeys(nounB));
+		if (ancestor < 0) {
+			return null;
+		}
+		return String.join(" ", synonyms[ancestor].synset);
 	}
 
-	/**
-	 * Checks that parameters are not null, and that they belong to a rooted DAG.
-	 * @param nounA
-	 * @param nounB
-	 */
-	private void checkInputs(String nounA, String nounB) {
-		if (nounA == null || nounB == null) throw new NullPointerException();
-		
-		if (!nounsMap.containsKey(nounA) || !nounsMap.containsKey(nounB)) throw new IllegalArgumentException();
-		
-	}
-	
-	/**
-	 * Checks that the String is not null
-	 * @param word
-	 */
-	private void checkInput(String word) {
-		if (word == null) throw new NullPointerException();
+	private List<Synonym> lookupNoun(String word) {
+		return symbolTable.get(word);
 	}
 
-	/**
-	 * do unit testing of this class
-	 * 
-	 * @param args
-	 */
+	private static String readSynonym(WordNet net) {
+		String word = StdIn.readString();
+		if (word.isEmpty()) {
+			return null;
+		}
+		while (!net.isNoun(word)) {
+			StdOut.printf("%s is not a noun\n", word);
+			word = StdIn.readString();
+		}
+		for (Synonym synonym : net.lookupNoun(word)) {
+			StdOut.printf("%d - synonyms: %s\n", synonym.id, String.join(", ", synonym.synset));
+		}
+		return word;
+	}
+
+	// do unit testing of this class
 	public static void main(String[] args) {
-		WordNet wordNet = new WordNet("/resources/synsets.txt", "/resources/hypernyms.txt");
-		Scanner in = new Scanner(System.in);
-		String word1 = in.nextLine();
-		String word2 = in.nextLine();
-		System.out.println("The sap of " + word1 + " and " + word2 + " is " + wordNet.sap(word1, word2));
-		System.out.println("Successfully finished");
+		if ((args == null) || (args.length < 2)) {
+			throw new IllegalArgumentException();
+		}
+		WordNet net = new WordNet(args[0], args[1]);
+		StdOut.printf("Found %s nouns\n", net.count());
+		while (!StdIn.isEmpty()) {
+			String wordA = readSynonym(net);
+			if (wordA == null) {
+				return;
+			}
+			String wordB = readSynonym(net);
+			if (wordB == null) {
+				return;
+			}
+			int length = net.distance(wordA, wordB);
+			String ancestor = net.sap(wordA, wordB);
+			StdOut.printf("length = %d, ancestor = %s\n", length, ancestor);
+		}
 	}
 }
