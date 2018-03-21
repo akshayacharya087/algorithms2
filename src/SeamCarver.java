6f6179cf6cf461c711f1690aa2f915d2550f2652
Yeremy Turcios <yeremyturcios@gmail.com>
2018-03-20 22:45:08 -0600

    Writing energy method.

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -2,10 +2,11 @@ import edu.princeton.cs.algs4.Picture;
 
 public class SeamCarver
 {
+    private Picture picture;
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture)
     {
-
+        this.picture = picture;
     }
 
     // current picture
@@ -17,19 +18,79 @@ public class SeamCarver
     // width of current picture
     public int width()
     {
-        return 0;
+        return this.picture.width();
     }
 
     // height of current picture
     public int height()
     {
-        return 0;
+        return this.picture.height();
     }
 
     // energy of pixel at column x and row y
     public double energy(int x, int y)
     {
-        return 0;
+        if (!isValidPixel(x, y)) throw new IllegalArgumentException();
+        int xGradient = calculateXGradient(x, y);
+        int yGradient = calculateYGradient(x, y);
+        return Math.sqrt(xGradient + yGradient);
+    }
+
+    private boolean isValidPixel(int x, int y)
+    {
+        return (x >= 0 && y <= width() - 1);
+    }
+
+    private int calculateXGradient(int x, int y)
+    {
+        int redXRight = 0; int greenXRight = 0; int blueXRight = 0;
+        int redXLeft = 0; int greenXLeft = 0; int blueXLeft = 0;
+
+        if (isValidPixel(x + 1, y))
+        {
+            int rgb = this.picture.getRGB(x + 1, y);
+            redXRight  = (rgb >> 16) & 0XFF;
+            greenXRight = (rgb >> 8) & 0XFF;
+            blueXRight  = (rgb) & 0xff;
+        }
+
+        if (isValidPixel(x - 1, y))
+        {
+            int rgb = this.picture.getRGB(x - 1, y);
+            redXLeft  = (rgb >> 16) & 0XFF;
+            greenXLeft = (rgb >> 8) & 0XFF;
+            blueXLeft  = (rgb) & 0xff;
+        }
+
+        return (int) (Math.pow(redXRight - redXLeft, 2) +
+                Math.pow(greenXRight - greenXLeft, 2) +
+                Math.pow(blueXRight - blueXLeft, 2));
+    }
+
+    private int calculateYGradient(int x, int y)
+    {
+        int redYUp = 0; int greenYUp = 0; int blueYUp = 0;
+        int redYDown = 0; int greenYDown = 0; int blueYDown = 0;
+
+        if (isValidPixel(x, y + 1))
+        {
+            int rgb = this.picture.getRGB(x, y + 1);
+            redYDown  = (rgb >> 16) & 0XFF;
+            greenYDown = (rgb >> 8) & 0XFF;
+            blueYDown  = (rgb) & 0xff;
+        }
+
+        if (isValidPixel(x, y - 1))
+        {
+            int rgb = this.picture.getRGB(x, y - 1);
+            redYUp = (rgb >> 16) & 0XFF;
+            greenYUp = (rgb >> 8) & 0XFF;
+            blueYUp  = (rgb) & 0xff;
+        }
+
+        return (int) (Math.pow(redYDown - redYUp, 2) +
+                Math.pow(greenYDown - greenYUp, 2) +
+                Math.pow(blueYDown - blueYUp, 2));
     }
 
     // sequence of indices for horizontal seam
